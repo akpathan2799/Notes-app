3e5fc8c0160e8ec067aa313d6205783350cc209c
Adeebkhan Pathan <akpathan2799@gmail.com>
2024-02-13 23:41:57 +0530

    commit

--- Archived/archive.js
+++ Archived/archive.js
@@ -106,12 +106,12 @@ function restoreNote(index) {
 // delete note functionality
 function deleteNote(index) {
     const availabelNotes = JSON.parse(localStorage.getItem('archiveNotes'));
-    const note = availabelNotes.splice(index, 1);
     if(JSON.parse(localStorage.getItem('deleteNotes')) === null){
         JSON.parse(localStorage.setItem('deleteNotes',JSON.stringify([])));
     }
     const deleteNotes = JSON.parse(localStorage.getItem('deleteNotes'));
-
+    const note = availabelNotes.splice(index, 1);
+    deleteNotes.push(note[0]);
    
 
     localStorage.setItem('archiveNotes', JSON.stringify(availabelNotes))
